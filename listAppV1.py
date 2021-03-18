9c2e39eeb45cd80b4af2d109a810a36e9c814476
BenjzB <77694685+BenjzB@users.noreply.github.com>
2021-03-18 12:03:34 -0600

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -16,15 +16,24 @@ def mainProgram():
             print("Hello, there! Let's work with lists!")
             print("Choose one of the following options. Type a number ONLY!")
             choice = input("""1. Add to list,
-    2. Return the value at an index position
-    3. Random Choice
-    4. End Program  """)
+2. Add a bunch o' numbers
+3. Return the value at an index position
+4. Print contents of list
+5. Random Choice
+6. Linear Search
+7. End Program  """)
             if choice == "1":
                 addToList()
             elif choice == "2":
-                indexValues()
+                addABunch()
             elif choice == "3":
+                indexValues()
+            elif choice == "4":
+                print(myList)
+            elif choice == "5":
                 randomSearch()
+            elif choice == "6":
+                linearSearch()
             else:
                 print("The program will now end.  ")
                 break
@@ -36,6 +45,14 @@ def addToList():
     myList.append(newItem)
     print(int(newItem))
     print(myList)
+
+def addABunch():
+    print("We're gonna add a bunch of numbers!")
+    numToAdd = input("How many integers do you want to add?  ")
+    numRange = input("And how high would you like these numbers to go?  ")
+    for x in range(0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
+    print("Your list is now complete!")
     
 def indexValues():
     indexPos = input("At what index position would you like to look?  ")
@@ -44,6 +61,13 @@ def indexValues():
 def randomSearch():
     print("Here's a random value from your list!")
     print(myList[random.randint(0, len(myList)-1)])
+
+def linearSearch():
+    print("We're going to search through the list IN THE WORST WAY POSSIBLE!")
+    searchItem = input("What are you looking for number-wise  ")
+    for x in range(len(myList)):
+        if myList[x] == int(searchItem):
+            print("Your item is at index {}".format(x))
 
 if __name__ == "__main__":
     mainProgram()
